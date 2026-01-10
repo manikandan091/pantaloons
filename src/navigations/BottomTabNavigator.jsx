f274a3e2bdfff6272510b006c4bdfebfb81e49c3
Mani M <manikandan.m@codingmart.com>
2026-01-10 14:02:10 +0530

    refactor: update bottom tab navigation to use Ionicons and remove unused icon assets

--- src/navigations/BottomTabNavigator.jsx
+++ src/navigations/BottomTabNavigator.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screen/HomeScreen';
 import WishlistScreen from '../screen/WishlistScreen';
@@ -8,11 +9,7 @@ import ProfileScreen from '../screen/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
-import HomeIconActive from '../assets/icons/menu_active.png';
-import HomeIconInactive from '../assets/icons/menu_inactive.png';
-import WishlistIconInactive from '../assets/icons/wishlist_inactive.png';
-import SearchIconInactive from '../assets/icons/search_inactive.png';
-import ProfileIconInactive from '../assets/icons/profile_inactive.png';
+
 
 const TabButton = (props) => {
     const { accessibilityState, style, children } = props;
@@ -56,10 +53,11 @@ const BottomTabNavigator = () => {
                 name="Home"
                 component={HomeScreen}
                 options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={focused ? HomeIconActive : HomeIconInactive}
-                            style={{ width: 24, height: 24, resizeMode: 'contain', marginTop: 8 }}
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons
+                            name={focused ? 'grid' : 'grid-outline'}
+                            size={24}
+                            color={"#00B0B5"}
                         />
                     ),
                     tabBarButton: (props) => <TabButton {...props} />,
@@ -69,10 +67,11 @@ const BottomTabNavigator = () => {
                 name="Wishlist"
                 component={WishlistScreen}
                 options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={focused ? HomeIconActive : WishlistIconInactive}
-                            style={{ width: 24, height: 24, resizeMode: 'contain', marginTop: 8 }}
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons
+                            name={focused ? 'heart' : 'heart-outline'}
+                            size={24}
+                            color={"#00B0B5"}
                         />
                     ),
                     tabBarButton: (props) => <TabButton {...props} />,
@@ -82,10 +81,11 @@ const BottomTabNavigator = () => {
                 name="Search"
                 component={SearchScreen}
                 options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={focused ? HomeIconActive : SearchIconInactive}
-                            style={{ width: 24, height: 24, resizeMode: 'contain', marginTop: 8 }}
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons
+                            name={focused ? 'search' : 'search-outline'}
+                            size={24}
+                            color={"#00B0B5"}
                         />
                     ),
                     tabBarButton: (props) => <TabButton {...props} />,
@@ -94,11 +94,12 @@ const BottomTabNavigator = () => {
             <Tab.Screen
                 name="Profile"
                 component={ProfileScreen}
-               options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={focused ? HomeIconActive : ProfileIconInactive}
-                            style={{ width: 24, height: 24, resizeMode: 'contain', marginTop: 8 }}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons
+                            name={focused ? 'person' : 'person-outline'}
+                            size={24}
+                            color={"#00B0B5"}
                         />
                     ),
                     tabBarButton: (props) => <TabButton {...props} />,
